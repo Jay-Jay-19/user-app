008b064ca4bb723e4db65713316a1ae6faa02244
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-06-20 00:31:41 +0200

    create userlist page

---
+++ src/pages/UserList.jsx
@@ -0,0 +1,12 @@
+import UserTable from '../components/UserTable';
+
+const UserList = () => {
+  return (
+    <div>
+      <h1>User List</h1>
+        <UserTable />
+    </div>
+  );
+};
+
+export default UserList;
