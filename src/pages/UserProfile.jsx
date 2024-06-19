ce8348ec96927536ad39972724b4c767c1794ccd
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-06-20 01:13:57 +0200

    create user profile page

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const UserProfile = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const fetchId = async() => {
+      try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+        const data = await res.json();
+        setUser([...data]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchId();
+
+    const fetchAlbums = async() => {
+      try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}/albums`);
+        const data = await res.json();
+        setAlbums([...data]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAlbums();
+  }, [id]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{user.name}'s Profile</h1>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <h2>Albums</h2>
+      <ul>
+        {albums.map(album => (
+          <li key={album.id}>
+            <Link to={`/album/${album.id}`}>{album.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <Link to="/">Back to User List</Link>
+    </div>
+  );
+};
+
+export default UserProfile;
